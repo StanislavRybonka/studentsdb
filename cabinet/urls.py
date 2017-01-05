21b933a39d6b294608345eb6df107101fe74310c
StanislavRybonka <stanislav1991r@gmail.com>
2017-01-05 23:08:44 +0200

    Work with urls

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -1,11 +1,23 @@
 from django.conf.urls import include, url
 
-from .views import StudentsDataBaseView, StudentsGroupView, CountVisitsView, StudentsEditView
+from cabinet import views
 
 urlpatterns = [
-    url(r'^students/$', StudentsDataBaseView.as_view(), name="students_db"),
-    url(r'^$', StudentsGroupView.as_view(), name="students_group"),
-    url(r'^visits/$', CountVisitsView.as_view(), name="count_visits"),
-    url(r'^edit/$', StudentsEditView.as_view(), name="edit_students")
+    # Students urls
+    url(r'^students/$', views.StudentsDataBaseView.as_view(), name='students_list'),
+    url(r'^students/add/$', views.StudentsAddView.as_view(), name='add_new_student'),
+    url(r'^edit/$', views.StudentsEditView.as_view(), name='edit_student'),
+    url(r'^students/delete/$', views.StudentsDeleteView.as_view(), name='delete_student'),
+
+    # Groups urls
+    url(r'^groups/$', views.GroupsView.as_view(), name="groups"),
+    url(r'^groups/add/$', views.GroupsAddView.as_view(), name='add_groups'),
+    url(r'^groups/123/edit/$', views.GroupsEditView.as_view(), name='edit_groups'),
+    url(r'^groups/123/delete/$', views.GroupsDeleteView.as_view(), name='delete_groups'),
+
+    # Journal urls
+    url(r'^journal/$', views.JournalView.as_view(), name="journal"),
+    url(r'^journal/123$', views.JournalSpecificStudentView.as_view(), name="journal_specific_student"),
+    url(r'^journal/update/$', views.JournalUpdateView.as_view(), name="journal_update"),
 
 ]
--- cabinet/views.py
+++ cabinet/views.py
@@ -2,17 +2,48 @@ from django.shortcuts import render
 from django.views import generic
 
 
+# Manage Students
 class StudentsDataBaseView(generic.TemplateView):
-    template_name = 'students_database.html'
+    template_name = 'students/students_list.html'
 
 
-class StudentsGroupView(generic.TemplateView):
-    template_name = "students_group.html"
-
-
-class CountVisitsView(generic.TemplateView):
-    template_name = "count_visits.html"
+class StudentsAddView(generic.TemplateView):
+    template_name = 'students/student_add_form.html'
 
 
 class StudentsEditView(generic.TemplateView):
-    template_name = "students_edit_form.html"
+    template_name = "students/students_edit_form.html"
+
+
+class StudentsDeleteView(generic.TemplateView):
+    template_name = 'students/students_delete.html'
+
+
+# Manage Groups
+class GroupsView(generic.TemplateView):
+    template_name = "groups/groups.html"
+
+
+class GroupsAddView(generic.TemplateView):
+    template_name = 'groups/groups_add.html'
+
+
+class GroupsEditView(generic.TemplateView):
+    template_name = 'groups/groups_edit.html'
+
+
+class GroupsDeleteView(generic.TemplateView):
+    template_name = 'groups/groups_delete.html'
+
+
+# Manage Journal
+class JournalView(generic.TemplateView):
+    template_name = "journal/journal.html"
+
+
+class JournalSpecificStudentView(generic.TemplateView):
+    template_name = 'journal/journal_specific_student.html'
+
+
+class JournalUpdateView(generic.TemplateView):
+    template_name = 'journal/journal_update.html'
