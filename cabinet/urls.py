7af7d51ccaf2fc710459bfeb84c2d3589e393618
StanislavRybonka <stanislav1991r@gmail.com>
2017-01-07 15:20:20 +0200

    Changed templates, urls, added some new simple function for return some content in template, added regex to urls patterns

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -4,20 +4,20 @@ from cabinet import views
 
 urlpatterns = [
     # Students urls
-    url(r'^students/$', views.StudentsDataBaseView.as_view(), name='students_list'),
-    url(r'^students/add/$', views.StudentsAddView.as_view(), name='add_new_student'),
-    url(r'^edit/$', views.StudentsEditView.as_view(), name='edit_student'),
-    url(r'^students/delete/$', views.StudentsDeleteView.as_view(), name='delete_student'),
+    url(r'^$', views.StudentsListView.as_view(), name='students_list'),
+    url(r'^students/add/$', views.StudentsAddView.as_view(), name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', views.StudentsEditView.as_view(), name='students_edit'),
+    url(r'^students/(?P<sid>\d+)delete/$', views.StudentsDeleteView.as_view(), name='students_delete'),
 
     # Groups urls
-    url(r'^groups/$', views.GroupsView.as_view(), name="groups"),
-    url(r'^groups/add/$', views.GroupsAddView.as_view(), name='add_groups'),
-    url(r'^groups/123/edit/$', views.GroupsEditView.as_view(), name='edit_groups'),
-    url(r'^groups/123/delete/$', views.GroupsDeleteView.as_view(), name='delete_groups'),
+    url(r'^groups/$', views.GroupsListView.as_view(), name="groups_list"),
+    url(r'^groups/add/$', views.GroupsAddView.as_view(), name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', views.GroupsEditView.as_view(), name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$', views.GroupsDeleteView.as_view(), name='groups_delete'),
 
     # Journal urls
     url(r'^journal/$', views.JournalView.as_view(), name="journal"),
-    url(r'^journal/123$', views.JournalSpecificStudentView.as_view(), name="journal_specific_student"),
+    url(r'^journal/(?P<jid>\d+)/$', views.JournalSpecificStudentView.as_view(), name="journal_specific_student"),
     url(r'^journal/update/$', views.JournalUpdateView.as_view(), name="journal_update"),
 
 ]
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponse
 
 
 # Manage Students
-class StudentsDataBaseView(generic.TemplateView):
-    template_name = 'students/students_list.html'
+class StudentsListView(generic.TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        students = {
+                       'id': 1,
+                       'first_name': 'Stanislav',
+                       'last_name': 'Rybonka',
+                       'ticket': 255,
+                       'image': 'img/foto1.jpg'
+                   },
+        return render(request, 'students/students_list.html', {'students': students})
 
 
 class StudentsAddView(generic.TemplateView):
@@ -14,13 +24,22 @@ class StudentsAddView(generic.TemplateView):
 class StudentsEditView(generic.TemplateView):
     template_name = "students/students_edit_form.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(StudentsEditView, self).get_context_data(**kwargs)
+
+        context['sid'] = self.kwargs.get('sid')
+        return context
+
 
 class StudentsDeleteView(generic.TemplateView):
     template_name = 'students/students_delete.html'
 
+    def students_delete(self, request, sid):
+        return render(self.request, sid)
+
 
 # Manage Groups
-class GroupsView(generic.TemplateView):
+class GroupsListView(generic.TemplateView):
     template_name = "groups/groups.html"
 
 
@@ -31,9 +50,15 @@ class GroupsAddView(generic.TemplateView):
 class GroupsEditView(generic.TemplateView):
     template_name = 'groups/groups_edit.html'
 
+    def groups_edit(self, request, gid):
+        return render(self.request, gid)
+
 
 class GroupsDeleteView(generic.TemplateView):
     template_name = 'groups/groups_delete.html'
+
+    def groups_delete(self, request, gid):
+        return render(self.request, gid)
 
 
 # Manage Journal
@@ -43,6 +68,9 @@ class JournalView(generic.TemplateView):
 
 class JournalSpecificStudentView(generic.TemplateView):
     template_name = 'journal/journal_specific_student.html'
+
+    def get_student(self, request, jid):
+        return render(self, request, jid)
 
 
 class JournalUpdateView(generic.TemplateView):
